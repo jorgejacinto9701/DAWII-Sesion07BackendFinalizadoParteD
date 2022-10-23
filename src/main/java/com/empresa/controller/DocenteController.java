bb3fb9e2dfb1b74d22ad6b9a62da4353e7e7b47b
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-10-23 13:21:52 -0500

    Ajustes

--- src/main/java/com/empresa/controller/DocenteController.java
+++ src/main/java/com/empresa/controller/DocenteController.java
@@ -10,8 +10,10 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,7 +56,13 @@ public class DocenteController {
 
 	@GetMapping("/listaDocenteConParametros")
 	@ResponseBody
-	public ResponseEntity<Map<String, Object>> listaDocenteNombreDniUbigeo(String nombre,String dni,int idUbigeo, int estado, String fechaInicio, String fechaFin) {
+	public ResponseEntity<Map<String, Object>> listaDocenteNombreDniUbigeo(
+			@RequestParam(name = "nombre", required = false, defaultValue = "") String nombre,
+			@RequestParam(name = "dni", required = false, defaultValue = "")String dni,
+			@RequestParam(name = "idUbigeo", required = false, defaultValue = "-1")int idUbigeo, 
+			@RequestParam(name = "estado", required = true)int estado, 
+			@RequestParam(name = "fechaInicio", required = false, defaultValue = "9999-01-01")String fechaInicio, 
+			@RequestParam(name = "fechaFin", required = false, defaultValue = "9999-01-01")String fechaFin) {
 		Map<String, Object> salida = new HashMap<>();
 		try {
 			List<Docente> lista = docenteService.listaDocentePorNombreDniUbigeo("%"+nombre+"%", dni, idUbigeo, estado,fechaInicio, fechaFin);
@@ -71,6 +79,24 @@ public class DocenteController {
 		return ResponseEntity.ok(salida);
 	}
 	
+	
+	@PutMapping
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> actualizaDocente(@RequestBody Docente obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			Docente objSalida = docenteService.insertaActualizaDocente(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", Constantes.MENSAJE_ACT_ERROR);
+			} else {
+				salida.put("mensaje", Constantes.MENSAJE_ACT_EXITOSO);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", Constantes.MENSAJE_ACT_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
 
 }
 
--- src/main/java/com/empresa/repository/DocenteRepository.java
+++ src/main/java/com/empresa/repository/DocenteRepository.java
@@ -12,7 +12,9 @@ public interface DocenteRepository extends JpaRepository<Docente, Integer> {
 	//JPQL
 	//Query no con tablas sino con clases que tienen @Entity
 	
-	@Query("select x from Docente x")    
+	@Query("select x from Docente x where (x.nombre like ?1) and (?2 is '' or x.dni = ?2) "
+			+ " and (?3 is -1 or x.ubigeo.idUbigeo =?3) and (x.estado = ?4) "
+			+ " and (?5 is '9999-01-01' or ?6 is '9999-01-01' or (x.fechaRegistro between ?5 and ?6))")    
 	public List<Docente> listaDocentePorNombreDniUbigeo(String nombre, String dni, int idUbigeo, int estado, String fecInicio, String fecFin);
 	
 }
